3a7b6a36d3199d68cb62e84b72b03f98c5f72d26
karmax-1989 <cacordoba@unicauca.edu.co>
2025-07-31 20:18:22 -0500

    Con tabla CSV Energias Dashboard

---
+++ dashboard/script.js
@@ -0,0 +1,30 @@
+async function cargarCSV() {
+
+
+    const respuesta = await fetch('./tabla.csv');
+
+    const texto = await respuesta.text();
+    const lineas = texto.trim().split("\n");
+    const encabezados = lineas[0].split(",");
+    const datos = lineas.slice(1039);
+
+    const tbody = document.querySelector("#tablaUsuarios tbody");
+
+    datos.forEach((fila) => {
+        const columnas = fila.split(",");
+
+        if (columnas.length === encabezados.length) {
+            const filasCeldas = document.createElement('tr');
+
+            columnas.forEach((dato) => {
+                const celda = document.createElement("td");
+                celda.textContent = dato.trim();
+                filasCeldas.appendChild(celda);
+            })
+            tbody.appendChild(filasCeldas);
+
+        }
+    }
+    )
+}
+cargarCSV();
